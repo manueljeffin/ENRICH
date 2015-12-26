f6b7e703dc07200488e51d58dbabbb00502e5f9c
Aparna-Lavanya <aparnalavanyak@gmail.com>
2015-12-26 13:15:18 +0530

    Update Determinant-GaussianElimination.c

--- Determinant-GaussianElimination.c
+++ Determinant-GaussianElimination.c
@@ -82,11 +82,11 @@ double determinant(double matrix[25][25], int order){
             for(j=0; j<order; j++){
 
                  if(i<j){
-                      multiplier=matrix[j][i]/matrix[i][i];
+                      multiplier=matrix[j][i];
 
                       for(k=0; k<order; k++){
 
-                           matrix[j][k] = matrix[j][k]-multiplier*matrix[i][k];
+                           matrix[j][k] = matrix[j][k]-multiplier*(matrix[i][k]/matrix[i][i]);
                       }
                  }
             }
