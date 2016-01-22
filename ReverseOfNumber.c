e948ce54b7a17c5556b3af5e9ce02288a15b882f
Aparna-Lavanya <aparnalavanyak@gmail.com>
2016-01-22 15:01:36 +0530

    Update ReverseOfNumber.c

--- ReverseOfNumber.c
+++ ReverseOfNumber.c
@@ -6,4 +6,31 @@
  Iteratively
  ========================================================================== */
 
+#include <stdio.h>
+
+int reverseNumber(int, int*);
+
+void main() {
+      int num, rev, digits = 0;
+
+	     printf("Enter a number: ");
+	     scanf("%d", &num);
+	
+	     rev = reverseNumber(num, &digits);
+
+	     printf("\nReversed number is: %0*d\n", digits, rev);   //prints the leading zeros too
+}
+
+int reverseNumber(int num, int* digits) {
+	    int rev = 0;
+
+	    while (num > 0) {
+	    
+		        rev = rev*10 + num % 10;
+		        num = num / 10;
+		        (*digits)++;      
+	}
+
+	   return rev;
+}
 
